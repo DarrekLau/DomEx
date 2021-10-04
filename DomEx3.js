4d840e671889b87cadedf217d23f8308b2614183
Darrek Lau <Lau.kok.tiong@gmail.com>
2021-10-04 18:58:05 +0800

    second commit

--- DomEx3.js
+++ DomEx3.js
@@ -4,3 +4,4 @@ function gettingLink() {
   alert(links[0]);
   alert(links[links.length - 1]);
 }
+// the link[0] is use as the array start from 0. It should show the title or name of the link rather than the number 0. 
